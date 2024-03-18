f375dac2da9d5d890307feeeaaed85631cb16b6f
Jxshvfx <spiteyhd@gmail.com>
2024-03-18 13:45:42 -0400

    swiper update

--- joshua-melody-app-festival-art/js/script.js
+++ joshua-melody-app-festival-art/js/script.js
@@ -20,3 +20,21 @@ const swiper2 = new Swiper('.swiper-featured-artworks', {
 
 });
 
+var swiper3 = new Swiper('.swiper-screenshots', {
+    parallax: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            opacity: 50,
+        },
+    }
+});
+// Opacité ne marche pas
