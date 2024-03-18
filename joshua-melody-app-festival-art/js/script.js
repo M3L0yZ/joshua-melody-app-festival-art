a04ee8e6ac4bf41f9eb2ba67e7f329538ec77cde
Melody C. <112108060+M3L0yZ@users.noreply.github.com>
2024-03-18 13:40:10 -0400

    commit-application

---
+++ joshua-melody-app-festival-art/js/script.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-filters', {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 4,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+
+});
+
+const swiper2 = new Swiper('.swiper-featured-artworks', {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: "2.2",
+  spaceBetween: 4,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+
+});
+
---
+++ joshua-melody-app-festival-art/js/scriptArt.js
@@ -0,0 +1,95 @@
+const oeuvres = {
+  oeuvre01: {
+    titre: "Retina",
+    artiste: "Herman Kolgen",
+    brefArtiste: "Artiste multidisciplinaire reconnu depuis plus de trente ans pour ses créations en arts médiatiques, Herman Kolgen vit et travaille à Montréal.",
+    aPropos: "Combien d'images notre acuité visuelle aura-t-elle enregistrées, produites et compilées tout au long de notre vie, elle qui, dépendant de la lumière, interprète à chaque instant des fragments du monde? C'est à travers notre iris caméra et notre filtre biologique que commencent le codage et la transmission au cerveau des traces lumineuses qui viendront se fixer ou non sur notre mémoire. Via cette frontière ouverte qu'est notre oeil, Herman Kolgen sonde la coexistence de l'humain et de ses territoires intermédiaires. Un double rapport au réel à la fois intime et extérieur dans tout ce qu'il a de plus fragile, de plus perméable et de plus nerveux. À se demander, entre bombardements de photons et fréquences optiques, ce qu'il restera de nos instants diffractés et de nos flashs éphémères. Un souvenir à la fois net et flou ? Une amnésie rétinienne ou une empreinte indélébile ?",
+    infoArtiste: "Sculpteur audiocinétique, il puise sa matière première dans la relation intime entre le son et l'image. Kolgen crée des pièces qui prennent la forme d'installations, d'œuvres vidéo et cinématographiques, de performances et de sculptures sonores. Il travaille dans un cycle constant d'exploration, à la croisée de différents médias, pour évoquer un nouveau langage technique et une esthétique singulière. L'impact des territoires sur la vie humaine est au cœur de ses recherches conceptuelles. Les tensions brutales qui en résultent ainsi que l'interaction entre divers éléments constituent l'épicentre de sa pratique. Son travail aux multiples facettes se caractérise par une approche radiographique. C'est cet effet de rayon X, avec sa qualité immatérielle, qui permet de voir l'invisible.",
+    imageArtiste: "../assets/artists/herman_kolgen.jpg",
+    imageOeuvre: "url('../assets/artworks/retina.jpg')",
+  },
+  oeuvre02: {
+    titre: "House of Moiré",
+    artiste: "Chloé Alexandra Thompson, Matthew Edwards",
+    brefArtiste: "Chloe Alexandra Thompson et Matthew Edwards sont des artistes interdisciplinaires.",
+    aPropos: "House of Moiré est un mariage entre op-art et immersion. Dans cette oeuvre linéaire et atmosphérique non-jeu, l'invité·e traverse les pièces d'une curieuse maison, laissant porter son regard et son attention vers les révélations d'une architecture psychique et d'objets sonores spatialisés.",
+    infoArtiste: "Chloé est une compositrice et une conceptrice sonore qui s'intéresse à la physicalité du son et à la médiation de la technologie, tandis que Matthew mêle l'installation, la performance et la réalité virtuelle.",
+    imageArtiste: "../assets/artists/chloe_matthews.jpg",
+    imageOeuvre: "url('../assets/artworks/house-of-moire.jpg')",
+  },
+  oeuvre03: {
+    titre: "Mega Touch",
+    artiste: "Håkan Lidbo",
+    brefArtiste: "Après une carrière dans la musique et la sortie de centaines de disques, Hakan Lidbo crée désormais de l'art interactif et de l'art sonore.",
+    aPropos: "La musique est invisible. Et lorsque la musique est terminée, il n'y a aucune preuve qu'elle n'ait jamais existé, sauf dans l'esprit de l'auditeur. Cette installation visualise et donne forme à la musique par le toucher de vos doigts. Une grande surface d'interaction, divisée en 400 sections qui déclenchent toutes différentes boucles musicales et animations qui expriment chorégraphiquement la musique dans cette position. Ce n'est pas un instrument à jouer ou à maîtriser. C'est un instrument à explorer. Touchez et trouvez des morceaux musicaux qui sonnent bien ensemble. Et quand vous le faites, n'oubliez pas d'écouter. 3 écrans tactiles, 3 ordinateurs, 6 haut-parleurs et un logiciel qui synchronisent toutes les boucles musicales et visuelles en une seule danse collaborative.",
+    infoArtiste: "Il crée aussi des innovations, des applications, des jeux, des livres, des films, de la radio, de l'architecture, de l'urbanisme, de la réalité virtuelle, de la technologie alimentaire, des keynotes, de l'éducation, de la robotique et installations pour musées, événements et lieux publics. Il est le fondateur du Rumtiden Idea Lab à Stockholm, où des experts de différentes disciplines explorent les aspects innovants de la musique, de l'art, de la science, de la société et de la technologie, fusionnés dans de nouvelles expressions.",
+    imageArtiste: "../assets/artists/hakan_lidbo.jpg",
+    imageOeuvre: "url('../assets/artworks/megatouch.jpg')",
+  },
+  oeuvre04: {
+    titre: "Pixelbloom - Timeless Butterflies",
+    artiste: "Artechouse",
+    brefArtiste: "Leader innovant dans le domaine de l'art numérique et expérientiel, ARTECHOUSE élargit les possibilités de l'art et la façon dont nous le vivons.",
+    aPropos: "Cette célébration annuelle embrasse la relation en constante évolution entre l'art, la technologie et la nature, repoussant les frontières et brouillant les frontières entre les mondes numérique et physique. Convergence de l'expression artistique, de l'innovation technique et des possibilités illimitées du domaine numérique, c'est une expérience transformatrice pour tous.",
+    infoArtiste: "L'équipe d'ARTECHOUSE a consacré la dernière décennie à soutenir la création d'un art expérientiel révolutionnaire, en poussant l'innovation de la narration à travers la plateforme d'exposition la plus avancée. Nous avons investi dans des ressources telles qu'une technologie de pointe, des outils de conception avancés, des espaces architecturaux uniques et une équipe d'experts passionnés pour créer un foyer permanent où les artistes et créatifs pionniers et axés sur la technologie pourraient expérimenter, développer et montrer. leur travail à de nouveaux publics.Depuis 2015, la vision de nos fondateurs Sandro & Tati consistant à redéfinir la façon dont l'art est vécu et à créer un écosystème de soutien pour l'avenir de l'art a permis à ARTECHOUSE de se démarquer en tant que pionnier dans le domaine de la création et de la présentation artistiques expérientielles aux États-Unis et dans le monde.",
+    imageArtiste: "../assets/artists/artechouse.jpg",
+    imageOeuvre: "url('../assets/artworks/pixelbloom.jpg')",
+  },
+  oeuvre05: {
+    titre: "Projet Bretez",
+    artiste: "Mylène Pardoen",
+    brefArtiste: "Conceptrice du Projet Bretez, elle est experte scientifique pour Chantier CNRS pour la restauration de Notre-Dame (acoustique, numérique).",
+    aPropos: "Simulation acoustique dans une reconstitution virtuelle d'un quartier de Paris au 18ème siècle",
+    infoArtiste: "Elle a participé aux Innovatives SHS 2015, 2017 et 2019. Outre Bretez et NOMADHISS, elle participe à de nombreux travaux de restitution virtuelle. Elle a lancé un nouveau projet ESPHAISTOSS, qui valorise le patrimoine culturel ommatériel par le sensoriel (notamment les métiers du bâti et de l'artisanat d'art).",
+    imageArtiste: "../assets/artists/mylene_pardoen.jpg",
+    imageOeuvre: "url('../assets/artworks/projet_bretez.jpg')",
+  },
+  oeuvre06: {
+    titre: "The Fourth Wall",
+    artiste: "Mahboobeh Kalaee",
+    brefArtiste: "Très curieuse et observatrice, Mahboobeh est inspirée et guidée par ce qui se passe dans sa vie et celle de ceux qui l'entourent.",
+    aPropos: "Une courte animation expérimentale se déroulant dans une « cuisine » qui reconfigure de manière créative la relation entre la maison, en tant qu'espace de vie, et la famille.",
+    infoArtiste: "Son film de fin d'études « The Fourth Wall », qu'elle a réalisé lors de son master en « Réalisation d'animation » à la Faculté de cinéma de l'Université de Téhéran, a été projeté à l'échelle internationale dans des festivals tels que Sundance et BFI Londres et a remporté de nombreux prix. S'appuyant sur des expériences du monde réel et utilisant son art pour exprimer ses pensées et ses idées uniques, Mahboobeh a l'ambition de travailler avec des marques pour créer un travail authentique et culturellement affectif.",
+    imageArtiste: "../assets/artists/mahboobeh_kalaee.png",
+    imageOeuvre: "url('../assets/artworks/the_fourth_wall.jpg')",
+  },
+}
+
+let oeuvreTitre = document.querySelector("h1");
+let nomArtiste = document.querySelector("h3");
+let nomArtisteModal = document.querySelector(".modal-title");
+let photoArtiste = document.querySelector(".card-img-top");
+let oeuvreImage = document.querySelector(".img-artwork");
+let brefArtiste = document.querySelector(".description");
+let infoArtiste = document.querySelector(".modal-body");
+let aPropos = document.querySelector(".apropos");
+
+function affichageOeuvre(oeuvre) {
+  oeuvreTitre.textContent = oeuvres[oeuvre].titre;
+  nomArtiste.textContent = oeuvres[oeuvre].artiste;
+  nomArtisteModal.textContent = oeuvres[oeuvre].artiste;
+  brefArtiste.textContent = oeuvres[oeuvre].brefArtiste;
+  infoArtiste.textContent = oeuvres[oeuvre].infoArtiste;
+  photoArtiste.src = oeuvres[oeuvre].imageArtiste;
+  oeuvreImage.style.backgroundImage = oeuvres[oeuvre].imageOeuvre;
+  aPropos.textContent = oeuvres[oeuvre].aPropos;
+}
+
+function detectionOeuvre(id) {
+  localStorage.setItem("oeuvre", id);
+}
+
+let numOeuvre = localStorage.getItem("oeuvre");
+affichageOeuvre(numOeuvre);
+
+function supprimerStorage() {
+  localStorage.clear();
+}
+
+
+let heart = document.querySelector(".coeur");
+heart.addEventListener("click", () =>{
+  heart.classList.toggle("toggle-on");
+})
+
+
